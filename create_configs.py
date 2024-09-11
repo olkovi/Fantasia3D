0539833ecc6f8a8cb915d950c21cf282c0339f54
olkovi <Ilya.Olkov@skoltech.ru>
2024-09-11 14:03:32 +0300

    appearance_configs

--- create_configs.py
+++ create_configs.py
@@ -10,6 +10,7 @@ os.makedirs("configs_orig1", exist_ok=True)
 os.makedirs("configs_orig2", exist_ok=True)
 os.makedirs("configs_edit1", exist_ok=True)
 os.makedirs("configs_edit2", exist_ok=True)
+os.makedirs("configs_appearance", exist_ok=True)
 
 # --- 3. Configuration Generation ---
 chair_config = {
@@ -38,15 +39,49 @@ chair_config = {
 "guidance_weight": 50
 }  # Load chair.json into a Python dictionary
 
+appearance_config = {
+    "mode": "appearance_modeling",
+    "base_mesh": "out/sandcastle/dmtet_mesh/mesh.obj",
+    "random_textures": "true",
+    "iter": 2000,
+    "coarse_iter": 300,
+    "save_interval": 100,
+    "texture_res": [ 2048, 2048 ],
+    "train_res": [512, 512],
+    "batch": 12,
+    "kd_min" : [0.03, 0.03, 0.03],
+    "kd_max" : [0.97, 0.97, 0.97],
+    "ks_min" : [0, 0.08, 0],
+    "ks_max" : [0, 0.9, 0],
+    "display": [{"latlong" : "true"}, {"bsdf" : "kd"}, {"bsdf" : "ks"}, {"bsdf" : "normal"}],
+    "envmap": "data/irrmaps/mud_road_puresky_4k.hdr",
+    "env_scale" : 2.0,
+    "train_background": "black",
+    "validate" : "true",
+    "out_dir": "sandcastle_appearance3",
+    "text" : "A highly detailed sandcastle",
+    "seed" : 42,
+    "add_directional_text": "true",
+    "camera_random_jitter": 0.3,
+    "fovy_range": [25.71, 45],
+    "elevation_range": [-10, 45],
+    "guidance_weight": 50,
+    "sds_weight_strategy": 2,
+    "early_time_step_range": [0.02, 0.98],
+    "late_time_step_range": [0.02, 0.98]
+}
+
+
 preprompt = "full frame, 3d model "
 outfold = os.path.join("out","negtest_" + "\'"+preprompt+"\'").replace(":","_")
 #os.makedirs(outfold, exist_ok=True)
 
 word_list=[]
-
+import copy
 for orig1_prompt, orig2_prompt in prompt_pairs:
     split1 = orig1_prompt.split()
     split2 = orig2_prompt.split()
+    configs = []
     if split1[0] not in word_list:
         firstword1 = split1[0]
         word_list.append(firstword1)
@@ -69,11 +104,13 @@ for orig1_prompt, orig2_prompt in prompt_pairs:
     # Modify chair_config['text'] for each pair
     chair_config["text"] = preprompt + orig1_prompt
     chair_config["out_dir"] = os.path.join(outfold, firstword1)
+    configs.append(copy.deepcopy(chair_config))
     with open(os.path.join("configs_orig1", orig1_filename), "w") as f:
         json.dump(chair_config, f, indent=4)  # Write base config
 
     chair_config["text"] = preprompt + orig2_prompt
     chair_config["out_dir"] = os.path.join(outfold, firstword2)
+    configs.append(copy.deepcopy(chair_config))
     with open(os.path.join("configs_orig2", orig2_filename), "w") as f:
         json.dump(chair_config, f, indent=4)  # Write edit config
 
@@ -82,6 +119,7 @@ for orig1_prompt, orig2_prompt in prompt_pairs:
     chair_config["out_dir"] = os.path.join(outfold, firstword2) +"_edit"
     chair_config["sdf_init_shape"] ="custom_mesh"
     chair_config["base_mesh"]=os.path.join("out",firstword1,"dmtet_mesh","mesh.obj").replace("\\","/")
+    configs.append(copy.deepcopy(chair_config))
     with open(os.path.join("configs_edit2", edit2_filename), "w") as f:
         json.dump(chair_config, f, indent=4)  # Write edit config
 
@@ -89,10 +127,21 @@ for orig1_prompt, orig2_prompt in prompt_pairs:
     chair_config["out_dir"] = os.path.join(outfold, firstword1) +"_edit"
     chair_config["sdf_init_shape"] ="custom_mesh"
     chair_config["base_mesh"]=os.path.join("out",firstword2,"dmtet_mesh","mesh.obj").replace("\\","/")
+    configs.append(copy.deepcopy(chair_config))
     with open(os.path.join("configs_edit1", edit1_filename), "w") as f:
         json.dump(chair_config, f, indent=4)  # Write edit config
 
     chair_config["sdf_init_shape"] = "ellipsoid"
     del chair_config["base_mesh"]
+    print(len(configs))
+    for config in configs:
+        appearance_config["text"] = config["text"]
+        appearance_config["base_mesh"] = os.path.join(config["out_dir"], "dmtet_mesh","mesh.obj").replace("\\","/")
+        appearance_config["out_dir"] = config["out_dir"] + "_appearance"
+        filename = appearance_config["out_dir"].split("\\")[-1] + ".json"
+        print(filename)
+        with open(os.path.join("configs_appearance", filename), "w") as f:
+            json.dump(appearance_config, f, indent=4)  # Write edited appearance config
+
 print(chair_config["out_dir"])
 print("Configuration files created successfully!")
